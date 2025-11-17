2f1075983c8a27dd9f76ff95ff2a5de4b46e5aee
Dio Prasetyo <prasetyodio04@gmail.com>
2025-11-17 16:16:04 +0700

    Update Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,24 +3,28 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import OurClientsSection from "./modules/consulting-service/our-client/section";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "Bootcamp Program — M-Knows Consulting",
-    description: "Bootcamp unggulan untuk pengembangan keterampilan dengan mentor berpengalaman di berbagai industri.",
+  title: "Bootcamp Program — M-Knows Consulting",
+  description:
+    "Bootcamp unggulan untuk pengembangan keterampilan dengan mentor berpengalaman di berbagai industri.",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <html lang="id">
-            <body className={inter.className}>
-                <Navbar />
-                <main>{children}</main>
-                {/* <OurClientsSection /> */}
-                <Footer />
-            </body>
-        </html>
-    );
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="id">
+      <body className={inter.className}>
+        <Navbar />
+        <main>{children}</main>
+        {/* <OurClientsSection /> */}
+        <Footer />
+      </body>
+    </html>
+  );
 }
